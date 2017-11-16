13d7662a95c01f998340e9042c5994c115996faa
Parth Patel <parth44@gmail.com>
2017-11-16 13:38:58 -0500

    databases

--- songbase.py
+++ songbase.py
@@ -1,11 +1,67 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+import os
+from flask import Flask, session, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dashfjkdfasasldjfklasjfuoeasm'
+app.config['SECRET_KEY'] = 'kdjfhkajfdljalsfjasl;j'
+
+# setup SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+db = SQLAlchemy(app)
+
+
+# define database tables
+class Artist(db.Model):
+    __tablename__ = 'artists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    about = db.Column(db.Text)
+    songs = db.relationship('Song', backref='artist')
+
+
+class Song(db.Model):
+    __tablename__ = 'songs'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256))
+    year = db.Column(db.Integer)
+    lyrics = db.Column(db.Text)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'))
+
 
 @app.route('/')
-def home():
+def index():
     # return '<h1>hello world!!!!</h1>'
     return render_template('index.html')
+
+
+@app.route('/artists')
+def show_all_artists():
+    artists = Artist.query.all()
+    return render_template('artist-all.html', artists=artists)
+
+
+# song-all.html adds song id to the edit button using a hidden input
+@app.route('/songs')
+def show_all_songs():
+    songs = Song.query.all()
+    return render_template('song-all.html', songs=songs)
+
+
+@app.route('/artist/add', methods=['GET', 'POST'])
+def add_artists():
+    if request.method == 'GET':
+        return render_template('artist-add.html')
+    if request.method == 'POST':
+        # get data from the form
+        name = request.form['name']
+        about = request.form['about']
+
+        # insert the data into the database
+        artist = Artist(name=name, about=about)
+        db.session.add(artist)
+        db.session.commit()
+        return redirect(url_for('show_all_artists'))
+
 
 
 @app.route('/form-demo', methods=['GET', 'POST'])
@@ -21,19 +77,31 @@ def form_demo():
         session['first_name'] = request.form['first_name']
         return redirect(url_for('form_demo'))
 
+@app.route('/mysong')
+def mysong():
+    return render_template('my-song.html')
+
+
+@app.route('/user/<string:name>/')
+def get_user(name):
+    # return 'hello %s your age is %d' % (name, 3)
+    return render_template('user.html', user_name=name)
+
+
 @app.route('/songs')
 def get_all_songs():
     songs = [
-        'song 1',
-        'song 2',
-        'song 3'
+        'song1',
+        'song2',
+        'song3'
     ]
     return render_template('songs.html', songs=songs)
 
-@app.route('/user/<string:name>')
-def get_user(name):
-    # return '<h1>Hello %s your age is %d</h1>' % (name,3)
-    return render_template('user.html',user_name=name)
 
-if __name__ =='__main__':
+@app.route('/users')
+def show_all_users():
+    return '<h2>this is the page for all users</h2>'
+
+
+if __name__ == '__main__':
     app.run()
